2fad762bf54befd0aef554262c4613b4903a1ccc
William Spada <william.spada@prideglobal.com>
2022-09-14 21:32:08 -0300

    finish endpoints

---
+++ src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,8 @@
+//18:00 -> 1680 min
+export function ConvertHourStringToMinutes(hourString : string) {
+  const [hours, minutes] = hourString.split(':').map(Number)
+
+  const minutesAmount = (hours*60)+minutes;
+
+  return minutesAmount;
+}
